2199289a493651b6d1ce8f8bfad44c4277156613
Vaibhavkin <nayekaksh352@gmail.com>
2025-06-21 17:49:42 +0530

    first commit

---
+++ frontend/backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// CORS enable if frontend on different port
+const cors = require('cors');
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.send('helm is deployed');
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Backend server is running at http://localhost:${port}`);
+});
+
